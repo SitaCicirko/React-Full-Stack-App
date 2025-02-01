22aae33bcd5bc641d100849cf2548f8e0b18be21
SitaCicirko <ns.cicirko@gmail.com>
2025-02-01 01:04:16 +0000

    7th commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Content from "./components/Content";
-import { Form } from "./components/Form";
+import Form from "./components/Form";
 
 export default function App() {
   return (
     <div>
+      <h1>Hiking Trails App</h1>
       <Routes>
-        <Route path="/" element={<Content />} />
-        <Route path="/add" element={<Form />} />
+        <Route element={<Content />} path="/" />
+        <Route element={<Form />} path="/add" />
       </Routes>
     </div>
   );
